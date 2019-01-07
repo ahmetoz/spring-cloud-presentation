e27f3dc91b64b096471d613d2d76d463863c6269
aoz <bilmuhahmet@gmail.com>
2019-01-07 21:45:45 +0100

    init account-service module

---
+++ account-service/src/main/java/com/presentation/account/AccountServiceApplication.java
@@ -0,0 +1,14 @@
+package com.presentation.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+}
+
