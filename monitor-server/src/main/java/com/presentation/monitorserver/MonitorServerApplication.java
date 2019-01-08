2f5483b46867e03d2866593a48ce6193adf91e32
aoz <bilmuhahmet@gmail.com>
2019-01-08 12:17:31 +0100

    add fallback monitor server

---
+++ monitor-server/src/main/java/com/presentation/monitorserver/MonitorServerApplication.java
@@ -0,0 +1,16 @@
+package com.presentation.monitorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class MonitorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorServerApplication.class, args);
+    }
+
+}
+
