2907550d04dc0f319b8a233a587c0f178af9ac50
aoz <bilmuhahmet@gmail.com>
2019-01-08 10:34:32 +0100

    add simple payment endpoint and call from order endpoint with normal rest client

--- order-service/src/main/java/com/presentation/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/presentation/order/OrderServiceApplication.java
@@ -2,11 +2,20 @@ package com.presentation.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class OrderServiceApplication {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
--- order-service/src/main/java/com/presentation/order/controller/OrderController.java
+++ order-service/src/main/java/com/presentation/order/controller/OrderController.java
@@ -2,10 +2,12 @@ package com.presentation.order.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
@@ -13,9 +15,15 @@ public class OrderController {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping(path = "/orders/{id}", method = RequestMethod.GET)
     public String createOrder(@PathVariable String id) {
         logger.info(String.format("order %s created", id));
+
+        String result = restTemplate.getForObject("http://localhost:8082/payment/"+ id, String.class);
+        logger.info(result);
 
         return "order-created";
     }
